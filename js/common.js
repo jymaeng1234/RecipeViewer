b858056c6b623f516c65359a88ef6214486e2ada
jymaeng <jymaeng1234@gmail.com>
2024-08-17 10:56:36 +0900

    [FEAT] API연결

---
+++ js/common.js
@@ -0,0 +1,83 @@
+const API_KEY = config.API_KEY;
+
+const $logo = document.getElementById("logo");
+const $banner = document.getElementById("banner");
+const $search = document.getElementById("inputSearch");
+const $searchBtn = document.getElementById("searchBtn");
+const $category = document.querySelector("category");
+const $cardListCon = document.getElementById("cardListCon");
+
+// const url = new URL(`http://openapi.foodsafetykorea.go.kr/api/${apiKey}/COOKRCP01/json/${startIdx}/${endIdx}`);
+const url = new URL(
+  `http://openapi.foodsafetykorea.go.kr/api/${API_KEY}/COOKRCP01/json`
+);
+let pageSize = 9;
+let page = 1;
+let totalResults = 0;
+let groupSize = 5;
+let currentPage = 1;
+
+//페이지네이션을 위한 변수
+let currentCategory = "반찬";
+let currentQuery = null;
+
+const fetchRecipes = async (startIdx = 1, endIdx = 9) => {
+  try {
+    let requestUrl = `${url}/${startIdx}/${endIdx}`;
+
+    console.log("url:", requestUrl);
+    const res = await fetch(requestUrl);
+    const data = await res.json();
+    console.log("data", data);
+
+    recipeList = data.COOKRCP01.row;
+    renderRecipes(recipeList);
+
+    console.log("ttt", recipeList);
+  } catch (e) {
+    console.error(e);
+  }
+};
+
+const renderRecipes = (recipeList) => {
+  console.log(".....", recipeList);
+  const recipeHtml = recipeList.map((recipe) => createHtml(recipe)).join("");
+  $cardListCon.innerHTML = recipeHtml;
+};
+
+const createHtml = (recipe) => {
+  let urlToImage = recipe.ATT_FILE_NO_MK || "./img/noimage.png";
+  let title = recipe.RCP_NM || "메뉴명 없음";
+  let calorie = recipe.INFO_ENG || "정보 없음";
+  let category = recipe.RCP_PAT2 || "정보 없음";
+
+  return `<div class="card" >
+            <img src="${urlToImage}" alt="" />
+            <h3>${title}</h3>
+            <div>
+              <strong>${calorie}kcal</strong>
+              <p>${category}</p>
+            </div>
+          </div>`;
+};
+
+function gotoMain() {
+  location.href = "/";
+}
+
+function showRullet() {}
+
+function searchRecipe() {}
+
+const loadData = () => {};
+
+const init = () => {
+  fetchRecipes();
+};
+
+init();
+
+$logo.addEventListener("click", gotoMain);
+$banner.addEventListener("click", showRullet);
+$search.addEventListener("keydown", searchRecipe);
+$searchBtn.addEventListener("click", searchRecipe);
